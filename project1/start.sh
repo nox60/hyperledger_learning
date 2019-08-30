4eb54ffdfbecc39e2ae73da2c874fceb78224800
Nox.Liu <nox60@hotmail.com>
2019-08-30 16:07:01 +0800

    Modified files

--- project1/start.sh
+++ project1/start.sh
@@ -1,47 +1,39 @@
 #!/bin/bash
 
-rm -rf /root/codes/temp/crypto-config
-rm -rf /root/codes/temp/orderer_ledger
-rm -rf /root/codes/temp/peer_ledger
-rm -rf /root/codes/temp/channel.tx
-rm -rf /root/codes/temp/genesis_block.pb
+rm -rf hyperledger_data
 
 ps -ef | grep orderer | grep -v grep | awk '{print $2}' | xargs kill -9
 ps -ef | grep peer | grep -v grep | awk '{print $2}' | xargs kill -9
 
 cryptogen generate \
 --config=./crypto-config.yaml \
---output="crypto-config"
+--output="hyperledger_data/crypto-config"
 
 echo 'Create genesis block'
 # 创世区块
-configtxgen -outputBlock genesis_block.pb \
+configtxgen -outputBlock hyperledger_data/genesis_block.pb \
 -profile TwoOrgsOrdererGenesis 
 
 echo 'Create tx'
 # tx
 configtxgen -profile TwoOrgsChannel \
--outputCreateChannelTx /root/codes/temp/channel.tx \
+-outputCreateChannelTx hyperledger_data/channel.tx \
 -channelID mychannel
 
 echo 'start orderer'
 # start orderer
-nohup orderer > orderer.log   2>&1 &
+nohup orderer > hyperledger_data/orderer.log   2>&1 &
 
-sleep 12
-
+sleep 2
 echo 'start peer'
-nohup peer node start > peer.log 2>&1 &
+nohup peer node start > hyperledger_data/peer.log 2>&1 &
 
-sleep 12
-
+sleep 2
 echo 'create channel'
 
 # create channel
 peer channel create -o orderer.test.com:7050 \
 -c mychannel \
--f /root/codes/temp/channel.tx \
+-f hyperledger_data/channel.tx \
 --tls true \
---cafile /root/codes/temp/crypto-config/ordererOrganizations/test.com/tlsca/tlsca.test.com-cert.pem
-
-
+--cafile hyperledger_data/crypto-config/ordererOrganizations/test.com/tlsca/tlsca.test.com-cert.pem

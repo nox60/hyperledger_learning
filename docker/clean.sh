d9d6d244cec6612a63b6ff18392cc85c7dae1ed3
Nox.Liu <nox60@hotmail.com>
2019-09-05 23:53:49 +0800

    a

--- docker/clean.sh
+++ docker/clean.sh
@@ -2,17 +2,10 @@
 
 
 docker rm -f orderer_container
-
 docker rm -f couchdb_org1
-
 docker rm -f org1_peer_0
-
 docker rm -f couchdb_org2
-
 docker rm -f org2_peer_0
-
 docker rm -f couchdb_org3
-
 docker rm -f org3_peer_0
-
 docker rm -f cli

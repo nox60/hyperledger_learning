091f1a40cb76504ebd4115ca23948c13325902d1
Nox.Liu <nox60@hotmail.com>
2019-09-05 16:09:55 +0800

    Update files

--- docker/start.sh
+++ docker/start.sh
@@ -2,7 +2,6 @@
 
 
 docker rm -f orderer_container
-
 docker run -it -d \
   --name orderer_container \
       -e username="ritchie"  \
@@ -29,14 +28,22 @@ docker run -it -d \
       hyperledger/fabric-orderer:1.4.3
 
 
-docker rm -f org1_peer_0
+docker rm -f couchdb_org1
+docker run -ti -d \
+--name couchdb_org1 \
+-e COUCHDB_USER=admin \
+-e COUCHDB_PASSWORD=dev@2019  \
+-v /root/codes/hyperledger_learning/docker/hyperledger_data/couchdb_org1/:/opt/couchdb/data  \
+-d hyperledger/fabric-couchdb  
 
+
+docker rm -f org1_peer_0
 docker run -it -d \
   --name org1_peer_0 \
       -e FABRIC_LOGGING_SPEC="INFO" \
       -e CORE_PEER_TLS_ENABLED="true" \
-      -e CORE_PEER_GOSSIP_USELEADERELECTION="true" \
-      -e CORE_PEER_GOSSIP_ORGLEADER="false" \
+      -e CORE_PEER_GOSSIP_USELEADERELECTION="false" \
+      -e CORE_PEER_GOSSIP_ORGLEADER="true" \
       -e CORE_PEER_PROFILE_ENABLED="true" \
       -e CORE_PEER_TLS_CERT_FILE="/etc/hyperledger/fabric/tls/server.crt" \
       -e CORE_PEER_TLS_KEY_FILE="/etc/hyperledger/fabric/tls/server.key" \
@@ -46,50 +53,71 @@ docker run -it -d \
       -e CORE_PEER_LISTENADDRESS="0.0.0.0:7051" \
       -e CORE_PEER_CHAINCODEADDRESS="peer0.org1.example.com:7052" \
       -e CORE_PEER_CHAINCODELISTENADDRESS="0.0.0.0:7052" \
-      -e CORE_PEER_GOSSIP_BOOTSTRAP="peer1.org1.example.com:8051" \
+      -e CORE_PEER_GOSSIP_BOOTSTRAP="peer0.org1.example.com:7051" \
       -e CORE_PEER_GOSSIP_EXTERNALENDPOINT="peer0.org1.example.com:7051" \
-      -e CORE_PEER_LOCALMSPID="Org1" \
+      -e CORE_PEER_LOCALMSPID="Org1MSP" \
+      -e CORE_LEDGER_STATE_STATEDATABASE="couchdb" \
+      -e CORE_LEDGER_STATE_COUCHDBCONFIG_COUCHDBADDRESS="couchdb:5984" \
+      -e CORE_LEDGER_STATE_COUCHDBCONFIG_USERNAME="admin" \
+      -e CORE_LEDGER_STATE_COUCHDBCONFIG_PASSWORD="dev@2019" \
       -e FABRIC_CFG_PATH="/etc/hyperledger/fabric" \
       -v /root/codes/hyperledger_learning/docker/hyperledger_data/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls:/etc/hyperledger/fabric/tls \
       -v /root/codes/hyperledger_learning/docker/hyperledger_data/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/msp:/etc/hyperledger/fabric/msp \
-      -v /root/codes/hyperledger_learning/docker/hyperledger_data:/var/hyperledger/production \
+      -v /root/codes/hyperledger_learning/docker/hyperledger_data/org1peer:/var/hyperledger/production \
       --link orderer_container:orderer.example.com \
+      --link couchdb_org1:couchdb \
       -p 7051:7051 \
       -p 7052:7052 \
       hyperledger/fabric-peer:1.4.3       
 
-docker rm -f org1_peer_1
 
+docker rm -f couchdb_org2
+docker run -ti -d \
+--name couchdb_org2 \
+-e COUCHDB_USER=admin \
+-e COUCHDB_PASSWORD=dev@2019  \
+-v /root/codes/hyperledger_learning/docker/hyperledger_data/couchdb_org1/:/opt/couchdb/data  \
+-d hyperledger/fabric-couchdb  
+
+
+docker rm -f org2_peer_0
 docker run -it -d \
-  --name org1_peer_1 \
+  --name org2_peer_0 \
       -e FABRIC_LOGGING_SPEC="INFO" \
       -e CORE_PEER_TLS_ENABLED="true" \
-      -e CORE_PEER_GOSSIP_USELEADERELECTION="true" \
-      -e CORE_PEER_GOSSIP_ORGLEADER="false" \
+      -e CORE_PEER_GOSSIP_USELEADERELECTION="false" \
+      -e CORE_PEER_GOSSIP_ORGLEADER="true" \
       -e CORE_PEER_PROFILE_ENABLED="true" \
       -e CORE_PEER_TLS_CERT_FILE="/etc/hyperledger/fabric/tls/server.crt" \
       -e CORE_PEER_TLS_KEY_FILE="/etc/hyperledger/fabric/tls/server.key" \
       -e CORE_PEER_TLS_ROOTCERT_FILE="/etc/hyperledger/fabric/tls/ca.crt" \
-      -e CORE_PEER_ID="peer1.org1.example.com" \
-      -e CORE_PEER_ADDRESS="peer1.org1.example.com:8051" \
-      -e CORE_PEER_LISTENADDRESS="0.0.0.0:8051" \
-      -e CORE_PEER_CHAINCODEADDRESS="peer1.org1.example.com:8052" \
-      -e CORE_PEER_CHAINCODELISTENADDRESS="0.0.0.0:8052" \
-      -e CORE_PEER_GOSSIP_BOOTSTRAP="peer0.org1.example.com:7051" \
-      -e CORE_PEER_GOSSIP_EXTERNALENDPOINT="peer1.org1.example.com:8051" \
-      -e CORE_PEER_LOCALMSPID="Org1" \
+      -e CORE_PEER_ID="peer0.org2.example.com" \
+      -e CORE_PEER_ADDRESS="peer0.org2.example.com:7151" \
+      -e CORE_PEER_LISTENADDRESS="0.0.0.0:7051" \
+      -e CORE_PEER_CHAINCODEADDRESS="peer0.org2.example.com:7052" \
+      -e CORE_PEER_CHAINCODELISTENADDRESS="0.0.0.0:7052" \
+      -e CORE_PEER_GOSSIP_BOOTSTRAP="peer0.org2.example.com:7051" \
+      -e CORE_PEER_GOSSIP_EXTERNALENDPOINT="peer0.org2.example.com:7051" \
+      -e CORE_PEER_LOCALMSPID="Org2MSP" \
+      -e CORE_LEDGER_STATE_STATEDATABASE="couchdb" \
+      -e CORE_LEDGER_STATE_COUCHDBCONFIG_COUCHDBADDRESS="couchdb:5984" \
+      -e CORE_LEDGER_STATE_COUCHDBCONFIG_USERNAME="admin" \
+      -e CORE_LEDGER_STATE_COUCHDBCONFIG_PASSWORD="dev@2019" \
       -e FABRIC_CFG_PATH="/etc/hyperledger/fabric" \
-      -v /root/codes/hyperledger_learning/docker/hyperledger_data/crypto-config/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls:/etc/hyperledger/fabric/tls \
-      -v /root/codes/hyperledger_learning/docker/hyperledger_data/crypto-config/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/msp:/etc/hyperledger/fabric/msp \
-      -v /root/codes/hyperledger_learning/docker/hyperledger_data:/var/hyperledger/production \
+      -v /root/codes/hyperledger_learning/docker/hyperledger_data/crypto-config/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls:/etc/hyperledger/fabric/tls \
+      -v /root/codes/hyperledger_learning/docker/hyperledger_data/crypto-config/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/msp:/etc/hyperledger/fabric/msp \
+      -v /root/codes/hyperledger_learning/docker/hyperledger_data/org2peer:/var/hyperledger/production \
       --link orderer_container:orderer.example.com \
-      -p 8051:8051 \
-      -p 8052:8052 \
+      --link couchdb_org2:couchdb \
+      -p 7151:7051 \
+      -p 7152:7052 \
       hyperledger/fabric-peer:1.4.3       
 
 
-docker rm -f cli
 
+
+
+docker rm -f cli
 docker run -it -d \
   --name cli \
       -e SYS_CHANNEL="byfn-sys-channel" \
@@ -112,5 +140,4 @@ docker run -it -d \
       -v /root/codes/hyperledger_learning/docker/hyperledger_data:/opt/channel-artifacts \
       --link orderer_container:orderer.example.com \
       --link org1_peer_0:peer0.org1.example.com \
-      --link org1_peer_1:peer1.org1.example.com \
       hyperledger/fabric-tools:1.4.3

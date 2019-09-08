6b9168418172db779761fc37189b7e4a93495bbd
Nox.Liu <nox60@hotmail.com>
2019-09-08 19:50:04 +0800

    Update files

--- docker/scripts.sh
+++ docker/scripts.sh
@@ -75,7 +75,7 @@ peer chaincode list \
 export CORE_PEER_LOCALMSPID=Org1MSP
 export CORE_PEER_TLS_ROOTCERT_FILE=/opt/crypto/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/ca.crt
 export CORE_PEER_MSPCONFIGPATH=/opt/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
-export CORE_PEER_ADDRESS=peer1.org1.example.com:7051
+export CORE_PEER_ADDRESS=peer1.org1.example.com:7351
 peer chaincode list \
 -C mychannel \
 --installed

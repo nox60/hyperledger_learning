7f060e3701fe1328390b409a7eb87d8792c59a25
Nox.Liu <nox60@hotmail.com>
2019-09-09 21:26:33 +0800

    Update files

--- docker2/start.sh
+++ docker2/start.sh
@@ -2,9 +2,9 @@
 
 docker rm -f $(docker ps -a | grep "dev-peer*" | awk '{print $1}')
 
-docker rm -f orderer.example.com
+docker rm -f orderer.dams.com
 docker run -it -d \
-  --name orderer.example.com \
+  --name orderer.dams.com \
       -e username="ritchie"  \
       -e FABRIC_LOGGING_SPEC="INFO" \
       -e ORDERER_GENERAL_LISTENADDRESS="0.0.0.0" \
@@ -21,8 +21,8 @@ docker run -it -d \
       -e ORDERER_GENERAL_CLUSTER_CLIENTCERTIFICATE="/var/hyperledger/orderer/tls/server.crt" \
       -e ORDERER_GENERAL_CLUSTER_CLIENTPRIVATEKEY="/var/hyperledger/orderer/tls/server.key" \
       -e ORDERER_GENERAL_CLUSTER_ROOTCAS="/var/hyperledger/orderer/tls/ca.crt" \
-      -v /root/codes/hyperledger_learning/docker/hyperledger_data/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp:/var/hyperledger/orderer/msp \
-      -v /root/codes/hyperledger_learning/docker/hyperledger_data/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/tls:/var/hyperledger/orderer/tls \
+      -v /root/codes/hyperledger_learning/docker/hyperledger_data/crypto-config/ordererOrganizations/dams.com/orderers/orderer.dams.com/msp:/var/hyperledger/orderer/msp \
+      -v /root/codes/hyperledger_learning/docker/hyperledger_data/crypto-config/ordererOrganizations/dams.com/orderers/orderer.dams.com/tls:/var/hyperledger/orderer/tls \
       -v /root/codes/hyperledger_learning/docker/hyperledger_data/orderer.genesis.block:/var/hyperledger/orderer/orderer.genesis.block \
       -v /root/codes/hyperledger_learning/docker/hyperledger_data:/var/hyperledger/production/orderer \
       -v /etc/hosts:/etc/hosts \
@@ -31,18 +31,18 @@ docker run -it -d \
       hyperledger/fabric-orderer:1.4.3
 
 
-docker rm -f couchdb_org1_peer0
+docker rm -f couchdb_cec_peer0
 docker run -ti -d \
---name couchdb_org1_peer0 \
+--name couchdb_cec_peer0 \
 -e COUCHDB_USER=admin \
 -e COUCHDB_PASSWORD=dev@2019  \
--v /root/codes/hyperledger_learning/docker/hyperledger_data/couchdb_org1/peer0:/opt/couchdb/data  \
+-v /root/codes/hyperledger_learning/docker/hyperledger_data/couchdb_cec/peer0:/opt/couchdb/data  \
 -d hyperledger/fabric-couchdb  
 
 
-docker rm -f peer0.org1.example.com
+docker rm -f peer0.cec.dams.com
 docker run -it -d \
-  --name peer0.org1.example.com \
+  --name peer0.cec.dams.com \
       -e FABRIC_LOGGING_SPEC="INFO" \
       -e CORE_PEER_TLS_ENABLED="true" \
       -e CORE_PEER_GOSSIP_USELEADERELECTION="true" \
@@ -51,43 +51,43 @@ docker run -it -d \
       -e CORE_PEER_TLS_CERT_FILE="/etc/hyperledger/fabric/tls/server.crt" \
       -e CORE_PEER_TLS_KEY_FILE="/etc/hyperledger/fabric/tls/server.key" \
       -e CORE_PEER_TLS_ROOTCERT_FILE="/etc/hyperledger/fabric/tls/ca.crt" \
-      -e CORE_PEER_ID="peer0.org1.example.com" \
-      -e CORE_PEER_ADDRESS="peer0.org1.example.com:7051" \
+      -e CORE_PEER_ID="peer0.cec.dams.com" \
+      -e CORE_PEER_ADDRESS="peer0.cec.dams.com:7051" \
       -e CORE_PEER_LISTENADDRESS="0.0.0.0:7051" \
-      -e CORE_PEER_CHAINCODEADDRESS="peer0.org1.example.com:7052" \
+      -e CORE_PEER_CHAINCODEADDRESS="peer0.cec.dams.com:7052" \
       -e CORE_PEER_CHAINCODELISTENADDRESS="0.0.0.0:7052" \
-      -e CORE_PEER_GOSSIP_BOOTSTRAP="peer1.org1.example.com:7351" \
-      -e CORE_PEER_GOSSIP_EXTERNALENDPOINT="peer0.org1.example.com:7051" \
-      -e CORE_PEER_LOCALMSPID="Org1MSP" \
+      -e CORE_PEER_GOSSIP_BOOTSTRAP="peer0.cec.dams.com:7351" \
+      -e CORE_PEER_GOSSIP_EXTERNALENDPOINT="peer0.cec.dams.com:7051" \
+      -e CORE_PEER_LOCALMSPID="cecMSP" \
       -e CORE_LEDGER_STATE_STATEDATABASE="couchdb" \
       -e CORE_LEDGER_STATE_COUCHDBCONFIG_COUCHDBADDRESS="couchdb:5984" \
       -e CORE_LEDGER_STATE_COUCHDBCONFIG_USERNAME="admin" \
       -e CORE_LEDGER_STATE_COUCHDBCONFIG_PASSWORD="dev@2019" \
       -e FABRIC_CFG_PATH="/etc/hyperledger/fabric" \
-      -v /root/codes/hyperledger_learning/docker/hyperledger_data/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls:/etc/hyperledger/fabric/tls \
-      -v /root/codes/hyperledger_learning/docker/hyperledger_data/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/msp:/etc/hyperledger/fabric/msp \
-      -v /root/codes/hyperledger_learning/docker/hyperledger_data/org1peer0:/var/hyperledger/production \
+      -v /root/codes/hyperledger_learning/docker/hyperledger_data/crypto-config/peerOrganizations/cec.dams.com/peers/peer0.cec.dams.com/tls:/etc/hyperledger/fabric/tls \
+      -v /root/codes/hyperledger_learning/docker/hyperledger_data/crypto-config/peerOrganizations/cec.dams.com/peers/peer0.cec.dams.com/msp:/etc/hyperledger/fabric/msp \
+      -v /root/codes/hyperledger_learning/docker/hyperledger_data/cecpeer0:/var/hyperledger/production \
       -v /etc/hosts:/etc/hosts \
       -v /var/run:/var/run \
-      --link couchdb_org1_peer0:couchdb \
+      --link couchdb_cec_peer0:couchdb \
       -p 7051:7051 \
       -p 7052:7052 \
       hyperledger/fabric-peer:1.4.3       
 
 
 
-docker rm -f couchdb_org1_peer1
+docker rm -f couchdb_ia3_peer0
 docker run -ti -d \
---name couchdb_org1_peer1 \
+--name couchdb_ia3_peer0 \
 -e COUCHDB_USER=admin \
 -e COUCHDB_PASSWORD=dev@2019  \
--v /root/codes/hyperledger_learning/docker/hyperledger_data/couchdb_org1_peer1/:/opt/couchdb/data  \
+-v /root/codes/hyperledger_learning/docker/hyperledger_data/couchdb_ia3_peer0/:/opt/couchdb/data  \
 -d hyperledger/fabric-couchdb  
 
 
-docker rm -f peer1.org1.example.com
+docker rm -f peer0.ia3.dams.com
 docker run -it -d \
-  --name peer1.org1.example.com \
+  --name peer0.ia3.dams.com \
       -e FABRIC_LOGGING_SPEC="INFO" \
       -e CORE_PEER_TLS_ENABLED="true" \
       -e CORE_PEER_GOSSIP_USELEADERELECTION="true" \
@@ -96,85 +96,42 @@ docker run -it -d \
       -e CORE_PEER_TLS_CERT_FILE="/etc/hyperledger/fabric/tls/server.crt" \
       -e CORE_PEER_TLS_KEY_FILE="/etc/hyperledger/fabric/tls/server.key" \
       -e CORE_PEER_TLS_ROOTCERT_FILE="/etc/hyperledger/fabric/tls/ca.crt" \
-      -e CORE_PEER_ID="peer1.org1.example.com" \
-      -e CORE_PEER_ADDRESS="peer1.org1.example.com:7051" \
+      -e CORE_PEER_ID="peer0.ia3.dams.com" \
+      -e CORE_PEER_ADDRESS="peer0.ia3.dams.com:7051" \
       -e CORE_PEER_LISTENADDRESS="0.0.0.0:7051" \
-      -e CORE_PEER_CHAINCODEADDRESS="peer1.org1.example.com:7352" \
+      -e CORE_PEER_CHAINCODEADDRESS="peer0.ia3.dams.com:7052" \
       -e CORE_PEER_CHAINCODELISTENADDRESS="0.0.0.0:7052" \
-      -e CORE_PEER_GOSSIP_BOOTSTRAP="peer0.org1.example.com:7051" \
-      -e CORE_PEER_GOSSIP_EXTERNALENDPOINT="peer1.org1.example.com:7351" \
-      -e CORE_PEER_LOCALMSPID="Org1MSP" \
+      -e CORE_PEER_GOSSIP_BOOTSTRAP="peer0.ia3.dams.com:7051" \
+      -e CORE_PEER_GOSSIP_EXTERNALENDPOINT="peer0.ia3.dams.com:7051" \
+      -e CORE_PEER_LOCALMSPID="ia3MSP" \
       -e CORE_LEDGER_STATE_STATEDATABASE="couchdb" \
       -e CORE_LEDGER_STATE_COUCHDBCONFIG_COUCHDBADDRESS="couchdb:5984" \
       -e CORE_LEDGER_STATE_COUCHDBCONFIG_USERNAME="admin" \
       -e CORE_LEDGER_STATE_COUCHDBCONFIG_PASSWORD="dev@2019" \
       -e FABRIC_CFG_PATH="/etc/hyperledger/fabric" \
-      -v /root/codes/hyperledger_learning/docker/hyperledger_data/crypto-config/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls:/etc/hyperledger/fabric/tls \
-      -v /root/codes/hyperledger_learning/docker/hyperledger_data/crypto-config/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/msp:/etc/hyperledger/fabric/msp \
-      -v /root/codes/hyperledger_learning/docker/hyperledger_data/org1peer1:/var/hyperledger/production \
+      -v /root/codes/hyperledger_learning/docker/hyperledger_data/crypto-config/peerOrganizations/ia3.dams.com/peers/peer0.ia3.dams.com/tls:/etc/hyperledger/fabric/tls \
+      -v /root/codes/hyperledger_learning/docker/hyperledger_data/crypto-config/peerOrganizations/ia3.dams.com/peers/peer0.ia3.dams.com/msp:/etc/hyperledger/fabric/msp \
+      -v /root/codes/hyperledger_learning/docker/hyperledger_data/ia3peer0:/var/hyperledger/production \
       -v /etc/hosts:/etc/hosts \
       -v /var/run:/var/run \
-      --link couchdb_org1_peer1:couchdb \
-      -p 7351:7051 \
-      -p 7352:7052 \
-      hyperledger/fabric-peer:1.4.3       
-
-
-docker rm -f couchdb_org2
-docker run -ti -d \
---name couchdb_org2 \
--e COUCHDB_USER=admin \
--e COUCHDB_PASSWORD=dev@2019  \
--v /root/codes/hyperledger_learning/docker/hyperledger_data/couchdb_org2/:/opt/couchdb/data  \
--d hyperledger/fabric-couchdb  
-
-
-docker rm -f peer0.org2.example.com
-docker run -it -d \
-  --name peer0.org2.example.com \
-      -e FABRIC_LOGGING_SPEC="INFO" \
-      -e CORE_PEER_TLS_ENABLED="true" \
-      -e CORE_PEER_GOSSIP_USELEADERELECTION="false" \
-      -e CORE_PEER_GOSSIP_ORGLEADER="true" \
-      -e CORE_PEER_PROFILE_ENABLED="true" \
-      -e CORE_PEER_TLS_CERT_FILE="/etc/hyperledger/fabric/tls/server.crt" \
-      -e CORE_PEER_TLS_KEY_FILE="/etc/hyperledger/fabric/tls/server.key" \
-      -e CORE_PEER_TLS_ROOTCERT_FILE="/etc/hyperledger/fabric/tls/ca.crt" \
-      -e CORE_PEER_ID="peer0.org2.example.com" \
-      -e CORE_PEER_ADDRESS="peer0.org2.example.com:7151" \
-      -e CORE_PEER_LISTENADDRESS="0.0.0.0:7051" \
-      -e CORE_PEER_CHAINCODEADDRESS="peer0.org2.example.com:7052" \
-      -e CORE_PEER_CHAINCODELISTENADDRESS="0.0.0.0:7052" \
-      -e CORE_PEER_GOSSIP_BOOTSTRAP="peer0.org2.example.com:7051" \
-      -e CORE_PEER_GOSSIP_EXTERNALENDPOINT="peer0.org2.example.com:7051" \
-      -e CORE_PEER_LOCALMSPID="Org2MSP" \
-      -e CORE_LEDGER_STATE_STATEDATABASE="couchdb" \
-      -e CORE_LEDGER_STATE_COUCHDBCONFIG_COUCHDBADDRESS="couchdb:5984" \
-      -e CORE_LEDGER_STATE_COUCHDBCONFIG_USERNAME="admin" \
-      -e CORE_LEDGER_STATE_COUCHDBCONFIG_PASSWORD="dev@2019" \
-      -e FABRIC_CFG_PATH="/etc/hyperledger/fabric" \
-      -v /root/codes/hyperledger_learning/docker/hyperledger_data/crypto-config/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls:/etc/hyperledger/fabric/tls \
-      -v /root/codes/hyperledger_learning/docker/hyperledger_data/crypto-config/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/msp:/etc/hyperledger/fabric/msp \
-      -v /root/codes/hyperledger_learning/docker/hyperledger_data/org2peer:/var/hyperledger/production \
-      -v /etc/hosts:/etc/hosts \
-      -v /var/run:/var/run \
-      --link couchdb_org2:couchdb \
+      --link couchdb_ia3_peer0:couchdb \
       -p 7151:7051 \
       -p 7152:7052 \
       hyperledger/fabric-peer:1.4.3       
 
 
-docker rm -f couchdb_org3
+docker rm -f couchdb_ic3
 docker run -ti -d \
---name couchdb_org3 \
+--name couchdb_ic3 \
 -e COUCHDB_USER=admin \
 -e COUCHDB_PASSWORD=dev@2019  \
--v /root/codes/hyperledger_learning/docker/hyperledger_data/couchdb_org3/:/opt/couchdb/data  \
+-v /root/codes/hyperledger_learning/docker/hyperledger_data/couchdb_ic3/:/opt/couchdb/data  \
 -d hyperledger/fabric-couchdb  
 
-docker rm -f peer0.org3.example.com
+
+docker rm -f peer0.ic3.dams.com
 docker run -it -d \
-  --name peer0.org3.example.com \
+  --name peer0.ic3.dams.com \
       -e FABRIC_LOGGING_SPEC="INFO" \
       -e CORE_PEER_TLS_ENABLED="true" \
       -e CORE_PEER_GOSSIP_USELEADERELECTION="false" \
@@ -183,27 +140,70 @@ docker run -it -d \
       -e CORE_PEER_TLS_CERT_FILE="/etc/hyperledger/fabric/tls/server.crt" \
       -e CORE_PEER_TLS_KEY_FILE="/etc/hyperledger/fabric/tls/server.key" \
       -e CORE_PEER_TLS_ROOTCERT_FILE="/etc/hyperledger/fabric/tls/ca.crt" \
-      -e CORE_PEER_ID="peer0.org3.example.com" \
-      -e CORE_PEER_ADDRESS="peer0.org3.example.com:7151" \
+      -e CORE_PEER_ID="peer0.ic3.dams.com" \
+      -e CORE_PEER_ADDRESS="peer0.ic3.dams.com:7151" \
       -e CORE_PEER_LISTENADDRESS="0.0.0.0:7051" \
-      -e CORE_PEER_CHAINCODEADDRESS="peer0.org3.example.com:7052" \
+      -e CORE_PEER_CHAINCODEADDRESS="peer0.ic3.dams.com:7052" \
       -e CORE_PEER_CHAINCODELISTENADDRESS="0.0.0.0:7052" \
-      -e CORE_PEER_GOSSIP_BOOTSTRAP="peer0.org3.example.com:7051" \
-      -e CORE_PEER_GOSSIP_EXTERNALENDPOINT="peer0.org3.example.com:7051" \
-      -e CORE_PEER_LOCALMSPID="Org3MSP" \
+      -e CORE_PEER_GOSSIP_BOOTSTRAP="peer0.ic3.dams.com:7051" \
+      -e CORE_PEER_GOSSIP_EXTERNALENDPOINT="peer0.ic3.dams.com:7051" \
+      -e CORE_PEER_LOCALMSPID="ic3MSP" \
       -e CORE_LEDGER_STATE_STATEDATABASE="couchdb" \
       -e CORE_LEDGER_STATE_COUCHDBCONFIG_COUCHDBADDRESS="couchdb:5984" \
       -e CORE_LEDGER_STATE_COUCHDBCONFIG_USERNAME="admin" \
       -e CORE_LEDGER_STATE_COUCHDBCONFIG_PASSWORD="dev@2019" \
       -e FABRIC_CFG_PATH="/etc/hyperledger/fabric" \
-      -v /root/codes/hyperledger_learning/docker/hyperledger_data/crypto-config/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls:/etc/hyperledger/fabric/tls \
-      -v /root/codes/hyperledger_learning/docker/hyperledger_data/crypto-config/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/msp:/etc/hyperledger/fabric/msp \
-      -v /root/codes/hyperledger_learning/docker/hyperledger_data/org3peer:/var/hyperledger/production \
+      -v /root/codes/hyperledger_learning/docker/hyperledger_data/crypto-config/peerOrganizations/ic3.dams.com/peers/peer0.ic3.dams.com/tls:/etc/hyperledger/fabric/tls \
+      -v /root/codes/hyperledger_learning/docker/hyperledger_data/crypto-config/peerOrganizations/ic3.dams.com/peers/peer0.ic3.dams.com/msp:/etc/hyperledger/fabric/msp \
+      -v /root/codes/hyperledger_learning/docker/hyperledger_data/ic3peer:/var/hyperledger/production \
       -v /etc/hosts:/etc/hosts \
       -v /var/run:/var/run \
-      --link couchdb_org3:couchdb \
+      --link couchdb_ic3:couchdb \
       -p 7251:7051 \
       -p 7252:7052 \
+      hyperledger/fabric-peer:1.4.3       
+
+
+docker rm -f couchdb_gov
+docker run -ti -d \
+--name couchdb_gov \
+-e COUCHDB_USER=admin \
+-e COUCHDB_PASSWORD=dev@2019  \
+-v /root/codes/hyperledger_learning/docker/hyperledger_data/couchdb_gov/:/opt/couchdb/data  \
+-d hyperledger/fabric-couchdb  
+
+docker rm -f peer0.gov.dams.com
+docker run -it -d \
+  --name peer0.gov.dams.com \
+      -e FABRIC_LOGGING_SPEC="INFO" \
+      -e CORE_PEER_TLS_ENABLED="true" \
+      -e CORE_PEER_GOSSIP_USELEADERELECTION="false" \
+      -e CORE_PEER_GOSSIP_ORGLEADER="true" \
+      -e CORE_PEER_PROFILE_ENABLED="true" \
+      -e CORE_PEER_TLS_CERT_FILE="/etc/hyperledger/fabric/tls/server.crt" \
+      -e CORE_PEER_TLS_KEY_FILE="/etc/hyperledger/fabric/tls/server.key" \
+      -e CORE_PEER_TLS_ROOTCERT_FILE="/etc/hyperledger/fabric/tls/ca.crt" \
+      -e CORE_PEER_ID="peer0.gov.dams.com" \
+      -e CORE_PEER_ADDRESS="peer0.gov.dams.com:7151" \
+      -e CORE_PEER_LISTENADDRESS="0.0.0.0:7051" \
+      -e CORE_PEER_CHAINCODEADDRESS="peer0.gov.dams.com:7052" \
+      -e CORE_PEER_CHAINCODELISTENADDRESS="0.0.0.0:7052" \
+      -e CORE_PEER_GOSSIP_BOOTSTRAP="peer0.gov.dams.com:7051" \
+      -e CORE_PEER_GOSSIP_EXTERNALENDPOINT="peer0.gov.dams.com:7051" \
+      -e CORE_PEER_LOCALMSPID="govMSP" \
+      -e CORE_LEDGER_STATE_STATEDATABASE="couchdb" \
+      -e CORE_LEDGER_STATE_COUCHDBCONFIG_COUCHDBADDRESS="couchdb:5984" \
+      -e CORE_LEDGER_STATE_COUCHDBCONFIG_USERNAME="admin" \
+      -e CORE_LEDGER_STATE_COUCHDBCONFIG_PASSWORD="dev@2019" \
+      -e FABRIC_CFG_PATH="/etc/hyperledger/fabric" \
+      -v /root/codes/hyperledger_learning/docker/hyperledger_data/crypto-config/peerOrganizations/gov.dams.com/peers/peer0.gov.dams.com/tls:/etc/hyperledger/fabric/tls \
+      -v /root/codes/hyperledger_learning/docker/hyperledger_data/crypto-config/peerOrganizations/gov.dams.com/peers/peer0.gov.dams.com/msp:/etc/hyperledger/fabric/msp \
+      -v /root/codes/hyperledger_learning/docker/hyperledger_data/govpeer:/var/hyperledger/production \
+      -v /etc/hosts:/etc/hosts \
+      -v /var/run:/var/run \
+      --link couchdb_gov:couchdb \
+      -p 7351:7051 \
+      -p 7352:7052 \
       hyperledger/fabric-peer:1.4.3       
 
 docker rm -f cli
@@ -214,13 +214,13 @@ docker run -it -d \
       -e CORE_VM_ENDPOINT="unix:///host/var/run/docker.sock" \
       -e FABRIC_LOGGING_SPEC="DEBUG" \
       -e CORE_PEER_ID="cli" \
-      -e CORE_PEER_ADDRESS="peer0.org1.example.com:7051" \
-      -e CORE_PEER_LOCALMSPID="Org1MSP" \
+      -e CORE_PEER_ADDRESS="peer0.cec.dams.com:7051" \
+      -e CORE_PEER_LOCALMSPID="cecMSP" \
       -e CORE_PEER_TLS_ENABLED="true"  \
-      -e CORE_PEER_TLS_CERT_FILE="/opt/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/server.crt" \
-      -e CORE_PEER_TLS_KEY_FILE="/opt/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/server.key" \
-      -e CORE_PEER_TLS_ROOTCERT_FILE="/opt/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" \
-      -e CORE_PEER_MSPCONFIGPATH="/opt/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" \
+      -e CORE_PEER_TLS_CERT_FILE="/opt/crypto/peerOrganizations/cec.dams.com/peers/peer0.cec.dams.com/tls/server.crt" \
+      -e CORE_PEER_TLS_KEY_FILE="/opt/crypto/peerOrganizations/cec.dams.com/peers/peer0.cec.dams.com/tls/server.key" \
+      -e CORE_PEER_TLS_ROOTCERT_FILE="/opt/crypto/peerOrganizations/cec.dams.com/peers/peer0.cec.dams.com/tls/ca.crt" \
+      -e CORE_PEER_MSPCONFIGPATH="/opt/crypto/peerOrganizations/cec.dams.com/users/Admin@cec.dams.com/msp" \
       -e PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/go/bin:/opt/gopath/bin" \
       -e GOROOT="/opt/go" \
       -e GOCACHE="off" \

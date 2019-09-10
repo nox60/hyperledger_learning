91d281a6dcdb0ea8bb2034b471ab399529be4107
Liu Li <nox60@hotmail.com>
2019-09-10 23:19:33 +0800

    Update files

---
+++ docker/clear.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+docker rm -f orderer.example.com
+docker rm -f peer0.org1.example.com
+docker rm -f peer1.org1.example.com
+docker rm -f peer0.org2.example.com
+docker rm -f peer0.org3.example.com
+docker rm -f couchdb_org1_peer0
+docker rm -f couchdb_org1_peer1
+docker rm -f couchdb_org2
+docker rm -f couchdb_org3
+docker rm -f cli
+
+
+

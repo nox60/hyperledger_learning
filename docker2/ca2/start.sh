5fd6c9bf305fb91c0bdc4889b5a6c6e8cda7b462
Nox.Liu <nox60@hotmail.com>
2019-10-01 09:11:53 +0800

    Add ca server script

---
+++ docker2/ca2/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker rm -f ca.dams.com
+
+docker run -it -d  \
+  --name ca.dams.com \
+      -p 7054:7054 \
+      -e FABRIC_CA_HOME="/opt/serverhome" \
+      -v /root/codes/hyperledger_learning/docker2/ca2/ca_server:/opt/serverhome \
+      hyperledger/fabric-ca:1.4.3

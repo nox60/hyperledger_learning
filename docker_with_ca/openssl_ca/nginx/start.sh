4a96764e98857451be8015c6fb61bfae0b3726b2
Nox.Liu <nox60@hotmail.com>
2019-10-03 21:49:47 +0800

    Modified fiels

--- docker_with_ca/openssl_ca/nginx/start.sh
+++ docker_with_ca/openssl_ca/nginx/start.sh
@@ -4,5 +4,6 @@ docker run -it -d \
 --name nginx_with_ssl \
 -v /root/codes/hyperledger_learning/docker_with_ca/openssl_ca/nginx/nginx_data/conf/nginx.conf:/etc/nginx/nginx.conf \
 -v /root/codes/hyperledger_learning/docker_with_ca/openssl_ca/nginx/nginx_data/ssl:/opt/ssl \
--p 80:80 \
+-v /root/codes/hyperledger_learning/docker_with_ca/openssl_ca/nginx/nginx_data/data:/opt/data \
+-p 80:88 \
 nginx

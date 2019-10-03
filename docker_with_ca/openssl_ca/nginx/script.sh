5f1306169abe9136a14298e11fe376ddf576d137
Nox.Liu <nox60@hotmail.com>
2019-10-03 22:45:24 +0800

    modify script.sh file

--- docker_with_ca/openssl_ca/nginx/script.sh
+++ docker_with_ca/openssl_ca/nginx/script.sh
@@ -6,3 +6,6 @@ openssl req -new -out simple.csr -key simple.key
 
 # 生成公钥, 此处crt文件
 openssl x509 -req -in simple.csr -out simple.crt -signkey simple.key -days 3650
+
+# 分析公钥文件
+openssl x509 -in  simple.crt -noout -text

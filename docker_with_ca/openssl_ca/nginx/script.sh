c72768ab37c05ebcb74b87a3db5518c4b2493f13
Nox.Liu <nox60@hotmail.com>
2019-10-03 22:26:30 +0800

    Update files

--- docker_with_ca/openssl_ca/nginx/script.sh
+++ docker_with_ca/openssl_ca/nginx/script.sh
@@ -1,5 +1,8 @@
+# 创建私钥
 openssl genrsa -out simple.key 2048
 
+# 创建证书请求
 openssl req -new -out simple.csr -key simple.key
 
+# 生成公钥, 此处crt文件
 openssl x509 -req -in simple.csr -out simple.crt -signkey simple.key -days 3650

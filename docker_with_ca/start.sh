b2489b741ba120de9db0f84b3401cf72fbfe01a7
Nox.Liu <nox60@hotmail.com>
2019-10-07 17:58:55 +0800

    add ca start scripts

--- docker_with_ca/start.sh
+++ docker_with_ca/start.sh
@@ -10,6 +10,37 @@ docker rm -f $(docker2 ps -a | grep "dev-peer*" | awk '{print $1}')
 
 docker rm -f orderer.dams.com
 
+export CEC_CA1_PRIVATE_KEY=$(cd /opt/local/codes/docker2/hyperledger_data/crypto-config/peerOrganizations/cec.dams.com/ca && ls *_sk)
+
+echo $CEC_CA1_PRIVATE_KEY
+
+docker run -it -d \
+  --name ca.cec.dams.com \
+      --network bc-net \
+
+
+services:
+  ca0:
+    image: hyperledger/fabric-ca:$IMAGE_TAG
+    environment:
+      - FABRIC_CA_HOME=/etc/hyperledger/fabric-ca-server
+      - FABRIC_CA_SERVER_CA_NAME=ca-org1
+      - FABRIC_CA_SERVER_TLS_ENABLED=true
+      - FABRIC_CA_SERVER_TLS_CERTFILE=/etc/hyperledger/fabric-ca-server-config/ca.org1.example.com-cert.pem
+      - FABRIC_CA_SERVER_TLS_KEYFILE=/etc/hyperledger/fabric-ca-server-config/${BYFN_CA1_PRIVATE_KEY}
+      - FABRIC_CA_SERVER_PORT=7054
+    ports:
+      - "7054:7054"
+    command: sh -c 'fabric-ca-server start --ca.certfile /etc/hyperledger/fabric-ca-server-config/ca.org1.example.com-cert.pem --ca.keyfile /etc/hyperledger/fabric-ca-server-config/${BYFN_CA1_PRIVATE_KEY} -b admin:adminpw -d'
+    volumes:
+      - ./crypto-config/peerOrganizations/org1.example.com/ca/:/etc/hyperledger/fabric-ca-server-config
+    container_name: ca_peerOrg1
+    networks:
+      - byfn
+
+
+
+
 docker run -it -d  \
   --name orderer.dams.com \
       --network bc-net \

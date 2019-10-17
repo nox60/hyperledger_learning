21bb5db6994044667bfe6fb6c04cc1ff5104faf1
Li.Liu <nox60@hotmail.com>
2019-10-17 23:50:16 +0800

    fix bugs

--- docker_with_ca_4/start.sh
+++ docker_with_ca_4/start.sh
@@ -239,18 +239,6 @@ fabric-ca-client register -d --id.name orderer1-org0 --id.secret ordererPW --id.
 
 
 
-export FABRIC_CA_CLIENT_TLS_CERTFILES=/tmp/hyperledger/tls-ca/crypto/tls-ca-cert.pem
-export FABRIC_CA_CLIENT_HOME=/tmp/hyperledger/tls-ca/admin
-fabric-ca-client enroll -d -u https://tls-ca-admin:tls-ca-adminpw@0.0.0.0:7052
-fabric-ca-client register -d --id.name peer1-org1 --id.secret peer1PW --id.type peer -u https://0.0.0.0:7052
-fabric-ca-client register -d --id.name peer2-org1 --id.secret peer2PW --id.type peer -u https://0.0.0.0:7052
-fabric-ca-client register -d --id.name peer1-org2 --id.secret peer1PW --id.type peer -u https://0.0.0.0:7052
-fabric-ca-client register -d --id.name peer2-org2 --id.secret peer2PW --id.type peer -u https://0.0.0.0:7052
-fabric-ca-client register -d --id.name orderer1-org0 --id.secret ordererPW --id.type orderer -u https://0.0.0.0:7052
-
-
-
-
 
 docker rm -f ca.ia3.dams.com
 docker run \

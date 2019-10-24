ebb41a5d83e717d8280e1d42aeb01fe51bcf0421
LiuLi <nox60@hotmail.com>
2019-10-24 18:04:20 +0800

    change some path to fix bug

--- docker_with_ca_4/clear.sh
+++ docker_with_ca_4/clear.sh
@@ -7,4 +7,4 @@ docker rm -f orderer.com peer0.cec.com couchdb_cec ca.cec ca.orderer ca.tls
 
 docker rmi -f $(docker images --format "{{.Repository}}" |grep "^dev-peer*")
 
-docker rm -f $(docker2 ps -a | grep "dev-peer*" | awk '{print $1}')
+docker rm -f $(docker ps -a | grep "dev-peer*" | awk '{print $1}')

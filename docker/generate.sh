d6744ca6acba4dcfd2548c5704867d7d3ef9b93a
Liu Li <nox60@hotmail.com>
2020-02-17 22:43:09 +0800

    Update start.sh script

--- docker/generate.sh
+++ docker/generate.sh
@@ -31,34 +31,3 @@ configtxgen -profile TwoOrgsChannel \
 
 
 
-
-
-
-----
-
-
-
-
-
-
-#!/bin/bash
-
-rm -rf hyperledger_data
-
-cryptogen generate \
---config=./crypto-config.yaml \
---output="hyperledger_data/crypto-config"
-
-echo 'Create genesis block'
-# 创世区块
-configtxgen -outputBlock hyperledger_data/orderer.genesis.block \
--profile TwoOrgsOrdererGenesis
-
-echo 'Create tx'
-# tx
-configtxgen -profile TwoOrgsChannel \
--outputCreateChannelTx hyperledger_data/channel.tx \
--channelID mychannel
-
-
-

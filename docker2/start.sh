2bf50743f56a8003a89afa35dbf52bd4262969f3
Liu Li <nox60@hotmail.com>
2020-06-06 23:41:11 +0800

    Update start.sh

--- docker2/start.sh
+++ docker2/start.sh
@@ -387,14 +387,3 @@ docker run -it -d \
       hyperledger/fabric-peer:1.4.3
 
 
-
-
-docker rm -f couchdb_ia3
-docker run -ti -d \
---name couchdb_ia3 \
---network bc-net \
--e COUCHDB_USER=admin \
--e COUCHDB_PASSWORD=dev@2019  \
--v /opt/local/codes/docker2/hyperledger_data/couchdb_ia3_peer0/:/opt/couchdb/data  \
--d hyperledger/fabric-couchdb
-

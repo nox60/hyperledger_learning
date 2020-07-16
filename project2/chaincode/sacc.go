33edf9e9acbccaab8776b4ffadcd89c542acfdc6
Liu Li <nox60@hotmail.com>
2020-07-16 17:53:22 +0800

    Update sacc.go

--- project2/chaincode/sacc.go
+++ project2/chaincode/sacc.go
@@ -202,20 +202,3 @@ func get(stub shim.ChaincodeStubInterface, args []string) (string, error) {
 	if len(args) != 1 {
 		return "", fmt.Errorf("Incorrect arguments. Expecting a key")
 	}
-
-	value, err := stub.GetState(args[0])
-	if err != nil {
-		return "", fmt.Errorf("Failed to get asset: %s with error: %s", args[0], err)
-	}
-	if value == nil {
-		return "", fmt.Errorf("Asset not found: %s", args[0])
-	}
-	return string(value), nil
-}
-
-// main function starts up the chaincode in the container during instantiate
-func main() {
-	if err := shim.Start(new(SimpleAsset)); err != nil {
-		fmt.Printf("Error starting SimpleAsset chaincode: %s", err)
-	}
-}
